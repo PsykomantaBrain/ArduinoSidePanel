0cd7c346cbf67c9804d4e9aff1f62e091077a11f
HarvesteR <felipe@floatingoriginstudios.com>
2020-10-21 16:35:29 -0500

    playing around with custom chars for the yoke LCD

--- Yoke/Yoke.ino
+++ Yoke/Yoke.ino
@@ -32,21 +32,73 @@ Joystick_ joystick = Joystick_(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTIC
 //the first parameter is the I2C address
 //the second parameter is how many rows are on your screen
 //the third parameter is how many columns are on your screen
-LiquidCrystal_I2C lcd(0x27, 17, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte cc0[8] = {
+    B11111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+};
+byte cc1[8] = {
+    B10000,
+    B01000,
+    B00100,
+    B00010,
+    B10001,
+    B01000,
+    B00000,
+    B00000,
+};
+byte cc2[8] = {
+    B00001,
+    B00010,
+    B00100,
+    B01000,
+    B10001,
+    B00010,    
+    B00000,
+    B00000,
+};
+byte cc3[8] = {
+    B11111,
+    B0000,
+    B00000,
+    B11111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+};
 
 void setup()
 {
     joystick.begin();
 
-    lcd.init();    
+    lcd.init();
+    lcd.createChar(0, cc0);    
+    lcd.createChar(1, cc1);   
+    lcd.createChar(2, cc2);   
+    lcd.createChar(3, cc3);   
+
+    
     lcd.backlight();
+    lcd.setBacklight(128);
+
     lcd.setCursor(0, 0);
-    lcd.print(" HRV SimViator ");
+    lcd.print(" HRV Simviator ");
+    lcd.setCursor(0, 1);
+    lcd.print("  \x08\x08\x03\x03\x01 \x02\x03\x03\x08\x08  ");
+
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
- 
+    
     delay(16);
 }
