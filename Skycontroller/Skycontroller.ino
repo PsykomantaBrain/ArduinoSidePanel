d5c1252abd47400a65422cad1d0db04997859206
HarvesteR <felipe@floatingorigininteractive.com>
2022-12-29 20:40:21 -0600

    Skycontroller board now sets up a HID joystick device with 8 btn and 8 axes, shows up as HRV Skycontroller on joy.cpl

--- Skycontroller/Skycontroller.ino
+++ Skycontroller/Skycontroller.ino
@@ -1,4 +1,29 @@
 
+
+#include "Joystick.h"
+#include "Mouse.h"
+
+#define sign(x) ((x) > 0 ? 1: ((x) < 0 ? -1 : 0))
+
+//High impedance pin mode
+#define OUTPUT_OFF INPUT_PULLUP
+
+// axis output 
+#define JOYSTICK_RANGE_MIN 0
+#define JOYSTICK_RANGE_MAX 1023
+
+Joystick_ joystick = Joystick_(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK, 
+								8, 0,				//btns, hats
+								true, true, true,	// X, Y, Z
+								true, true, true,	// Rx, Ry, Rz
+								true, true,			// rudder, throttle
+								false, false, false //accel, brake, steering
+							);
+
+
+
+
+
 // placeholder code, just flash 'HRV' in morse code on the builtin LED
 
 char* letters[] = {
@@ -11,9 +36,25 @@ char* text = "HRV";
 int dotDelay = 80;
 
 
+
+
+
 void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.begin(115200);
+	
+	analogReadResolution(12);
+
+	joystick.begin();
+	joystick.setXAxisRange(JOYSTICK_RANGE_MIN, JOYSTICK_RANGE_MAX);
+	joystick.setYAxisRange(JOYSTICK_RANGE_MIN, JOYSTICK_RANGE_MAX);
+	joystick.setZAxisRange(JOYSTICK_RANGE_MIN, JOYSTICK_RANGE_MAX);
+	joystick.setThrottleRange(JOYSTICK_RANGE_MIN, JOYSTICK_RANGE_MAX);
+	joystick.setRxAxisRange(JOYSTICK_RANGE_MIN, JOYSTICK_RANGE_MAX);
+	joystick.setRyAxisRange(JOYSTICK_RANGE_MIN, JOYSTICK_RANGE_MAX);
+
+	Mouse.begin();
+
+	
 }
 
 void loop()
